9e880e161babc8fcb56028747d8fa02a89dcd52b
Juan Marin <juan@codecery.com>
2014-01-28 11:07:49 -0800

    Handles GET, OPTIONS and POST

---
+++ client/scripts/app.js
@@ -0,0 +1,35 @@
+var getMessages = function() {
+  $.ajax({
+    url: 'http://127.0.0.1:8080/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to receive messages');
+    }
+  });
+};
+
+var postMessage = function(message) {
+  $.ajax({
+    // always use this url
+    url: 'http://127.0.0.1:8080/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.log(arguments)
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+$(document).ready(function(){
+  getMessages();
+});
--- server/request-handler.js
+++ server/request-handler.js
@@ -1,10 +1,29 @@
+var chatLog = [];
+
+chatLog.push({
+  createdAt: new Date().toJSON(),
+  updatedAt: new Date().toJSON(),
+  objectId: '00001',
+  roomname: 'Winterfell',
+  text: 'Winter is coming!',
+  username: 'Eddard Stark'
+});
+chatLog.push({
+  createdAt: new Date().toJSON(),
+  updatedAt: new Date().toJSON(),
+  objectId: '00002',
+  roomname: 'Winterfell',
+  text: 'Fear cuts deeper than swords!',
+  username: 'Arya Stark'
+});
+
 /* You should implement your request handler function in this file.
  * And hey! This is already getting passed to http.createServer()
  * in basic-server.js. But it won't work as is.
  * You'll have to figure out a way to export this function from
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
-
+var url = require("url");
 exports.handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
@@ -15,21 +34,47 @@ exports.handleRequest = function(request, response) {
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   var statusCode = 200;
+  var headers = defaultCorsHeaders;
+
+  if (request.url !== '/1/classes/chatterbox') {
+    statusCode = 418;
+    headers['Content-Type'] = "text/plain";
+    response.writeHead(statusCode, headers);
+    response.end("I'm a teapot!");
+  }
+
+  if(request.method === 'OPTIONS') {
+    headers['Content-Type'] = "text/plain";
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+
+  if (request.method === 'GET') {
+    headers['Content-Type'] = "application/json";
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(chatLog));
+  }
+
+  if (request.method === 'POST') {
+    headers['Content-Type'] = "application/json";
+    response.writeHead(statusCode, headers);
+    request.on('data', function(data){
+      chatLog.push(JSON.parse(data));
+      console.log(chatLog);
+      response.end(JSON.stringify(chatLog[chatLog.length - 1]));
+    });
+  }
 
   /* Without this line, this server wouldn't work. See the note
    * below about CORS. */
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "text/plain";
 
   /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
+
 
   /* Make sure to always call response.end() - Node will not send
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
